41c2f3fa60b543ef4ed8a4da42e5e5c287918b94
jliu12 <jliu12@stanford.edu>
2014-05-26 15:54:26 -0700

    classifier training data

---
+++ classifier-file-gen.py
@@ -0,0 +1,96 @@
+import re
+import os
+import sys
+import getopt
+import os.path
+import fnmatch
+import glob
+import collections
+import json
+import math
+import random
+import errno
+from itertools import tee, islice
+import nltk
+from nltk.tokenize import RegexpTokenizer
+from nltk import bigrams, trigrams
+
+
+corpusPath = ""
+#savePath = ""
+corpusPathRoot = ""
+
+
+def ensure_path(path):
+    try:
+        os.makedirs(path)
+    except OSError as exception:
+        if exception.errno != errno.EEXIST:
+            raise
+
+def find_ngrams(input_list, n):
+  	return zip(*[input_list[i:] for i in range(n)])
+
+
+def getRandomGram(length, line, unigrams, bigrams, trigrams):
+	neg_example = ""
+	while True:
+		if length == 1:
+			neg_example = random.choice(unigrams)
+		elif length == 2:
+			neg_example = " ".join(random.choice(bigrams))
+		elif length == 3:
+			neg_example = " ".join(random.choice(trigrams))
+
+		if neg_example != line:
+			break
+
+	#print "length: " + str(length) + ", pos: " + line + ", neg: " + neg_example
+	return neg_example
+
+def writeToFile():
+	#ensure_path (savePath)
+	path = "train.info"
+	write_file = open(path, "w")
+
+	for filename in glob.glob(corpusPath):
+		write_file.write(filename + "\n")
+		file_key_name = filename.split(".txt")[0] + ".key"
+
+		key_file = open(file_key_name, 'r')
+
+		negative_examples = []
+
+		words = (re.findall('\w+', open(filename).read().lower()))
+		bigrams = find_ngrams(words, 2)
+		trigrams = find_ngrams(words, 3)
+
+		for line in key_file:
+			length = len(line.split())
+			if length <= 3:
+				line = line.rstrip('\n')
+				write_file.write(line + ":1\n")
+				neg_example = getRandomGram(length, line, words, bigrams, trigrams)
+				negative_examples.append(neg_example)
+
+		for neg in negative_examples:
+			write_file.write(neg + ":0\n")
+
+		write_file.write("\n")
+
+	write_file.close()
+
+
+def main():
+	global corpusPath
+	global corpusPathRoot
+
+	#savePath = "results/classifier_files/" + sys.argv[1] + "/"
+	corpusPath = sys.argv[1] + "/*.txt"
+	corpusPathRoot = sys.argv[1] + "/"
+
+	writeToFile()
+
+
+if __name__ == "__main__":
+	main()
