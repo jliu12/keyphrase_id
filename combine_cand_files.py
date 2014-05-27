224638a79d8c06df9203a91438d7951e7e390526
Devney <devney@stanford.edu>
2014-05-26 20:20:29 -0700

    added testCandidatesV1.txt

--- combine_cand_files.py
+++ combine_cand_files.py
@@ -7,7 +7,7 @@ sourcePath = sourcePrefix + "*" + sourceSuffix
 
 corpusPrefix = "corpus/krapavin2000-train/"
 corpusSuffix = ".txt"
-targetPath = "trainCandidatesV1.txt"
+targetPath = "testCandidatesV1.txt"
 
 
 
