697679c9a5cd9226808bba2ed94954f636843e9a
jliu12 <jliu12@stanford.edu>
2014-06-08 16:57:56 -0700

    Added superinfo

--- classifier_file_super_gen.py
+++ classifier_file_super_gen.py
@@ -19,6 +19,7 @@ from nltk import bigrams, trigrams
 corpusPath = ""
 #savePath = ""
 corpusPathRoot = ""
+candsPath = "candidates/v600Candsv2/v600Candsv2Train/"
 
 
 def ensure_path(path):
@@ -32,21 +33,21 @@ def find_ngrams(input_list, n):
   	return zip(*[input_list[i:] for i in range(n)])
 
 
-def getRandomGram(length, line, unigrams, bigrams, trigrams):
-	neg_example = ""
-	while True:
-		if length == 1:
-			neg_example = random.choice(unigrams)
-		elif length == 2:
-			neg_example = " ".join(random.choice(bigrams))
-		elif length == 3:
-			neg_example = " ".join(random.choice(trigrams))
+def getRandomGram(cands, keys):
+	count = 0
+	neg_list = []
 
-		if neg_example != line:
-			break
+	while True:
+		neg_example = random.choice(cands)
+
+		if neg_example not in keys:
+			neg_list.append(neg_example)
+			count += 1
+			if (count == 3):
+				break
 
 	#print "length: " + str(length) + ", pos: " + line + ", neg: " + neg_example
-	return neg_example
+	return neg_list
 
 def file_len(fname):
     with open(fname) as f:
@@ -57,20 +58,30 @@ def file_len(fname):
 
 def writeToFile():
 	#ensure_path (savePath)
-	path = "train.info"
+	path = "train.superinfo"
 	write_file = open(path, "w")
 
 	for filename in glob.glob(corpusPath):
 		write_file.write(filename + "\n")
 		file_key_name = filename.split(".txt")[0] + ".key"
+		file_id_index = filename.rfind("/")
+		file_id = (filename[file_id_index + 1:])[0:-4]
+
+		print file_key_name
+		print file_id_index
+		print file_id
+
+		cands_file_name = candsPath + file_id + ".cand"
+
+		with open(cands_file_name) as f:
+			cands_lines = f.read().splitlines()
 
 		key_file = open(file_key_name, 'r')
 
-		negative_examples = []
+		with open(file_key_name) as f:
+			key_lines = f.read().splitlines()
 
-		words = (re.findall('\w+', open(filename).read().lower()))
-		bigrams = find_ngrams(words, 2)
-		trigrams = find_ngrams(words, 3)
+		negative_examples = []
 
 		num_keys = file_len(file_key_name)
 
@@ -79,7 +90,7 @@ def writeToFile():
 			if length <= 3:
 				line = line.rstrip('\n')
 				write_file.write(line + ":1\n")
-				neg_examples = getRandomGram(length, line, words, bigrams, trigrams)
+				neg_examples = getRandomGram(cands_lines, key_lines)
 				negative_examples.extend(neg_examples)
 
 		for neg in negative_examples:
